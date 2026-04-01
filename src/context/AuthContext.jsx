05ce3592a2533464f3c29c29079fed8ce8953966
minhajul islam <mm01882390860@gmail.com>
2026-04-01 13:40:21 +0600

    add project structure, routes, auth context and private route

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase.config";
+import { auth } from "../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext(null);
---
+++ src/pages/ForgetPassword/ForgetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ForgetPassword = () => {
+  return (
+    <div>
+      forgetPasword
+    </div>
+  )
+}
+
+export default ForgetPassword
